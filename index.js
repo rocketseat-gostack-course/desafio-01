0338dfc427f507f2ddbf81338732fac27ad80848
Marcelo XP <marceloxp@gmail.com>
2020-02-25 15:57:02 -0300

    - List projects.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.listen(3000);
