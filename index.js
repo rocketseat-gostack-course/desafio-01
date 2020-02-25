86545e0db3e508bbc03206b87c4534e1e375feae
Marcelo XP <marceloxp@gmail.com>
2020-02-25 15:57:59 -0300

    - Added create projects.

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+  const exists = projects.filter(item => item.id === id);
+  if (exists.length > 0) {
+    return res.status(400).json({ error: 'Project already exists.' });
+  }
+  projects.push({ id, title, tasks: [] });
+  return res.json(projects);
+});
+
 server.listen(3000);
